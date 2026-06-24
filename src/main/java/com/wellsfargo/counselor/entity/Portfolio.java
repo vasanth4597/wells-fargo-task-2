e2433ab6ac04f2c752c80e044f6b28fc2418487a
vasanth4597 <vasanth.ai1931@gmail.com>
2026-06-24 08:50:39 +0530

    Create Portfolio.java

---
+++ src/main/java/com/wellsfargo/counselor/entity/Portfolio.java
@@ -0,0 +1,29 @@
+package com.wellsfargo.counselor.entity;
+
+import jakarta.persistence.*;
+import java.util.List;
+
+@Entity
+public class Portfolio {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long portfolioId;
+
+    private String portfolioName;
+
+    @OneToOne
+    @JoinColumn(name = "client_id")
+    private Client client;
+
+    @OneToMany(mappedBy = "portfolio")
+    private List<Security> securities;
+
+    public Portfolio() {}
+
+    public Portfolio(String portfolioName) {
+        this.portfolioName = portfolioName;
+    }
+
+    // Generate Getters and Setters
+}
